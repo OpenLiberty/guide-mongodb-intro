17a0c0a5b52720f94c564c76ddc6a79168a9a7cc
Eveline Cai <33134427+evelinec@users.noreply.github.com>
2019-03-14 10:53:09 -0400

    Converted readme section to multipane (#22)
    * Converted readme section to multipane
    
    * Addressed Gilbert's review feedback
    
    * Minor changes
    
    * Adjusted length of some text
    
    * Added license header for sample app
    
    * Additional fixes template readme to work on the UI
    
    * Fixed hotspot and include file issues

--- finish/src/main/java/io/openliberty/guides/todolistSample/managers/samples/SampleTodoManager.java
+++ finish/src/main/java/io/openliberty/guides/todolistSample/managers/samples/SampleTodoManager.java
@@ -1,3 +1,15 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::copyright[]
 package io.openliberty.guides.todolistSample.managers.samples;
 
 import java.util.ArrayList;
@@ -46,7 +58,7 @@ public class SampleTodoManager implements TodoManager {
 	@Override
 	public Optional<TodoModel> updateTodo(Integer id, TodoModel updated) {
         Optional<TodoModel> todo = findTodo(id);
-        
+
         if (todo.isPresent()) {
             TodoModel tm = todo.get();
             tm.setTitle(updated.getTitle());
@@ -59,7 +71,7 @@ public class SampleTodoManager implements TodoManager {
 	@Override
 	public Optional<TodoModel> deleteTodo(Integer id) {
         Optional<TodoModel> todo = findTodo(id);
-        
+
         if (todo.isPresent()) {
             todos.remove(todo.get());
         }
--- start/src/main/java/io/openliberty/guides/todolistSample/Main.java
+++
@@ -1,9 +0,0 @@
-package io.openliberty.guides.todolistSample;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class Main extends Application {
-
-}
