9333eafde12d21785be36dadcff993fd1470ff97
Nimrah <46586499+NimG98@users.noreply.github.com>
2019-11-11 12:42:41 -0500

    Explain why commands are separate

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -14,16 +14,16 @@ set -euxo pipefail
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory. 
-
 mvn -q clean package liberty:create liberty:install-feature liberty:deploy
 
 
 ## Run the tests
+# These commands are separated because if one of the commands fail, the test script will fail and exit. 
+# e.g if liberty:start fails, then there is no need to run the failsafe commands. 
 #       liberty:start             - Start a Liberty server in the background.
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-
 mvn liberty:start
 mvn failsafe:integration-test liberty:stop
 mvn failsafe:verify
