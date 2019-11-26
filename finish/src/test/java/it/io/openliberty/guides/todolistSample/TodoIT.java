f718488db98df31e4a21d7270d0e790d6935065b
Nimrah <46586499+NimG98@users.noreply.github.com>
2019-11-26 16:00:02 -0500

    Dev mode updates (#27)
    * use ideal pom
    
    * add liberty config variables
    
    * rename test classes to IT
    
    * Update README.adoc
    
    * update with hide_tags
    
    * update readme with IT test name
    
    * fixed tag
    
    * fixed LMP version
    
    * added test execution order
    
    * make start subdir name same as finish

--- finish/src/test/java/it/io/openliberty/guides/todolistSample/TodoIT.java
+++ finish/src/test/java/it/io/openliberty/guides/todolistSample/TodoIT.java
@@ -12,22 +12,38 @@
  // end::copyright[]
 package it.io.openliberty.guides.todolistSample;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
-import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-public class TodoTest {
+@TestMethodOrder(OrderAnnotation.class)
+public class TodoIT {
+
     @Test
+    @Order(1)
     public void testGetTodos() throws Exception {
         HttpClient client = HttpClients.createDefault();
         HttpGet request = new HttpGet("http://localhost:9080/todo-app/todo");
         HttpResponse response = client.execute(request);
 
-        assertEquals(response.getStatusLine().getStatusCode(), 200);
+        assertEquals(200, response.getStatusLine().getStatusCode(), "Incorrect response code from url");
+    }
+
+    @Test
+    @Order(2)
+    public void testGetFirstTodo() throws Exception {
+        HttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:9080/todo-app/todo" + "/4");
+        HttpResponse response = client.execute(request);
+
+        assertEquals(200, response.getStatusLine().getStatusCode(), "Incorrect response code from url");
     }
 }
